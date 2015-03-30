d0d243a7629b102a62d82ab865f9bf9ec84409fb
lihengokok <lihengokok@gmail.com>
2015-03-29 20:10:03 -0400

    Junit Package added

---
+++ app/src/main/java/centralcpccommittee/shopwithfriends/test/ItemsOnMapActivityTest.java
@@ -0,0 +1,21 @@
+package centralcpccommittee.shopwithfriends.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by lihen_000 on 3/29/2015.
+ */
+public class ItemsOnMapActivityTest extends InstrumentationTestCase {
+
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+
+
+
+}
+
+
+
