ce8028e08ad6028f843803a3c345ea79b66aa5b4
Tino <tgong7@gatech.edu>
2015-04-24 05:03:03 -0400

    Fixed some bugs and added some animation between the activities

--- app/src/main/java/centralcpccommittee/shopwithfriends/SaleListActivity.java
+++ app/src/main/java/centralcpccommittee/shopwithfriends/SaleListActivity.java
@@ -3,14 +3,9 @@ package centralcpccommittee.shopwithfriends;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.util.Map;
 
 import centralcpccommittee.shopwithfriends.Presenter.SaleListPresenter;
 import centralcpccommittee.shopwithfriends.Presenter.SaleListPresenterImpl;
@@ -34,12 +29,14 @@ public class SaleListActivity extends ActionBarActivity implements SaleListView{
         move.putExtra("userEmail", userEmail);
         startActivity(move);
         finish();
+        overridePendingTransition(R.animator.push_down_in, R.animator.fadeout);
     }
     public void itemOnMapPressed(@SuppressWarnings("UnusedParameters") View view) {
         Intent move = new Intent(this, SalesOnMapActivity.class);
         move.putExtra("userEmail", userEmail);
         startActivity(move);
         finish();
+        
     }
     public void displayList(String[] items) {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.items_font,
